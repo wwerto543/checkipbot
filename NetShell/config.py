eb32b5c7fde9847241629d9a3b9f5b54a76ac3c2
wwerto543 <kovalevskiv2020@gmail.com>
2026-05-11 12:32:01 +0300

    Update config.py

--- NetShell/config.py
+++ NetShell/config.py
@@ -1,6 +1,6 @@
-import os  # Обязательно импортируем os, чтобы работал getenv
+import os  # Вот этой строки у тебя не хватает
 
-# Вытягиваем переменные из настроек хостинга
+# Основные настройки (берутся из переменных хостинга)
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
 CHANNEL_ID = os.getenv("CHANNEL_ID")
@@ -9,5 +9,5 @@ SHODAN_API = os.getenv("SHODAN_API")
 # Настройка БД
 DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL and DATABASE_URL.startswith("postgresql://"):
-    # Автоматически меняем на асинхронный драйвер
+    # Заменяем для работы с асинхронным драйвером asyncpg
     DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
